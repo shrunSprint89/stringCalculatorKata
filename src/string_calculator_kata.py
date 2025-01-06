ea3b0c7d9bdcae5065b81a52b192ff7e805368cc
Sharan Selvaraj <sharan.bizid@gmail.com>
2025-01-06 19:12:50 +0530

    Class refactor after passing test - create common method to extract numbers and delimiters to keep code DRY

--- src/string_calculator_kata.py
+++ src/string_calculator_kata.py
@@ -1,23 +1,28 @@
 class StringCalculator:
     def Add(self, numbers) -> int:
+        if numbers == "":
+            return 0
+        numbers, delimiters = self.__get_delimiters_and_numbers(numbers)
+        number_list = self.__split_numbers(numbers, delimiters)
+        return self.__calculate_sum(number_list)
+
+    def __get_delimiters_and_numbers(self, numbers):
         if numbers.startswith("//[") and "]" in numbers:
             closing_bracket_index = numbers.rfind("]")
             delimiters_section = numbers[3:closing_bracket_index]
             delimiters = delimiters_section.split("][")
             numbers = numbers[closing_bracket_index + 2:]
-            number_list = self.__split_numbers(numbers, delimiters)
-            return self.__calculate_sum(number_list)
+            return numbers, delimiters
+            
         if numbers.startswith("//"):
             delimiter = numbers[2]
             numbers = numbers[4:]
-            number_list = self.__split_numbers(numbers, [delimiter])
-            return self.__calculate_sum(number_list)
+            return numbers, [delimiter]
+            
         if numbers.isdigit():
-            return int(numbers)
-        if ',' in numbers or '\n' in numbers:
-            number_list = self.__split_numbers(numbers, [",","\n"])
-            return self.__calculate_sum(number_list)
-        return 0
+            return numbers, []
+            
+        return numbers, [",", "\n"]
 
     def __calculate_sum(self, number_list):
         sum = 0

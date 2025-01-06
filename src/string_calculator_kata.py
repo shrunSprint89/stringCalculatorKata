19ae44f91a4eefcbd5174e2b08fffd49c8265c91
Sharan Selvaraj <sharan.bizid@gmail.com>
2025-01-06 18:51:02 +0530

    Class refactor after passing test - Reuse common method for splitting with any set of delimiters

--- src/string_calculator_kata.py
+++ src/string_calculator_kata.py
@@ -4,17 +4,17 @@ class StringCalculator:
             delimiter_end = numbers.find("]")
             delimiter = numbers[3:delimiter_end]
             numbers = numbers[delimiter_end + 2:]
-            number_list = numbers.split(delimiter)
+            number_list = self.__split_numbers(numbers, [delimiter])
             return self.__calculate_sum(number_list)
         if numbers.startswith("//"):
             delimiter = numbers[2]
             numbers = numbers[4:]
-            number_list = numbers.split(delimiter)
+            number_list = self.__split_numbers(numbers, [delimiter])
             return self.__calculate_sum(number_list)
         if numbers.isdigit():
             return int(numbers)
         if ',' in numbers or '\n' in numbers:
-            number_list = numbers.replace('\n', ',').split(',')
+            number_list = self.__split_numbers(numbers, [",","\n"])
             return self.__calculate_sum(number_list)
         return 0
 
@@ -24,3 +24,8 @@ class StringCalculator:
             if int(num) <= 1000:
                 sum += int(num)
         return sum
+       
+    def __split_numbers(self, numbers, delimiters):
+        for delimiter in delimiters:
+            numbers = numbers.replace(delimiter, ',')
+        return numbers.split(',')

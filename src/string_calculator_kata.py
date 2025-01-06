b86b87f1c948302e591c7ffa5806d529cd22ca17
Sharan Selvaraj <sharan.bizid@gmail.com>
2025-01-06 17:41:41 +0530

    Class refactor - string with new line character as separator

--- src/string_calculator_kata.py
+++ src/string_calculator_kata.py
@@ -2,8 +2,8 @@ class StringCalculator:
     def Add(self, numbers) -> int:
         if numbers.isdigit():
             return int(numbers)
-        if ',' in numbers:
-            number_list = numbers.split(',')
+        if ',' in numbers or '\n' in numbers:
+            number_list = numbers.replace('\n', ',').split(',')
             sum = 0
             for num in number_list:
                 sum += int(num)

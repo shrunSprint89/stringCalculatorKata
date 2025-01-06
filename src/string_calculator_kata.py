2bc41246507b0255ac5dac57ab3eb92fe8b4c5ee
Sharan Selvaraj <sharan.bizid@gmail.com>
2025-01-06 17:50:10 +0530

    Class refactor - add custom delimiter

--- src/string_calculator_kata.py
+++ src/string_calculator_kata.py
@@ -1,5 +1,13 @@
 class StringCalculator:
     def Add(self, numbers) -> int:
+        if numbers.startswith("//"):
+            delimiter = numbers[2]
+            numbers = numbers[4:]
+            number_list = numbers.split(delimiter)
+            sum = 0
+            for num in number_list:
+                sum += int(num)
+            return sum
         if numbers.isdigit():
             return int(numbers)
         if ',' in numbers or '\n' in numbers:

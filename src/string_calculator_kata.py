85c4636441792b359cbfd59cc7c6c570e29ac6c9
Sharan Selvaraj <sharan.bizid@gmail.com>
2025-01-06 17:52:02 +0530

    Class refactor after passing test - Reuse common method for calculating sum

--- src/string_calculator_kata.py
+++ src/string_calculator_kata.py
@@ -4,16 +4,16 @@ class StringCalculator:
             delimiter = numbers[2]
             numbers = numbers[4:]
             number_list = numbers.split(delimiter)
-            sum = 0
-            for num in number_list:
-                sum += int(num)
-            return sum
+            return self.__calculate_sum(number_list)
         if numbers.isdigit():
             return int(numbers)
         if ',' in numbers or '\n' in numbers:
             number_list = numbers.replace('\n', ',').split(',')
-            sum = 0
-            for num in number_list:
-                sum += int(num)
-            return sum
+            return self.__calculate_sum(number_list)
         return 0
+
+    def __calculate_sum(self, number_list):
+        sum = 0
+        for num in number_list:
+            sum += int(num)
+        return sum

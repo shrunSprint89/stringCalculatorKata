4bae35a3e8bc9e2d2b42032d350a46b44cf050b4
Sharan Selvaraj <sharan.bizid@gmail.com>
2025-01-06 17:20:49 +0530

    Class refactor - string with two number to return the sum

--- src/string_calculator_kata.py
+++ src/string_calculator_kata.py
@@ -2,5 +2,10 @@ class StringCalculator:
     def Add(self, numbers) -> int:
         if numbers.isdigit():
             return int(numbers)
-            
+        if ',' in numbers:
+            number_list = numbers.split(',')
+            sum = 0
+            for num in number_list:
+                sum += int(num)
+            return sum
         return 0
